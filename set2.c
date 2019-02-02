d67e569c06cbbfacef77dd5597e5ce4e16e80e4c
SNivethini <46355009+SNivethini@users.noreply.github.com>
2019-02-02 00:39:38 -0800

    Create set2.c

---
+++ set2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int N, k, result;
+    scanf("%d", &N);
+    scanf("%d", &k);
+    result = pow(N,k);
+    printf("%d",result);
+    return 0;
+}
