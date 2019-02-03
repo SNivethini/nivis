6685dddac64b6d05de0cc29de9f486ad5b091f57
SNivethini <46355009+SNivethini@users.noreply.github.com>
2019-02-03 11:09:40 +0530

    Update fact.py

--- fact.py
+++ fact.py
@@ -2,10 +2,8 @@
 num = int(input())
 
 factorial = 1
-if num < 0:
-   print(" factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
+if num == 0:
+   print("1")
 else:
    for i in range(1,num + 1):
        factorial = factorial*i
