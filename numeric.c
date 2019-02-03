d865fa2c67281542e21ec6cf0e9993f15a1151cd
SNivethini <46355009+SNivethini@users.noreply.github.com>
2019-02-03 21:11:32 +0530

    Create numeric.c

---
+++ numeric.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+char s[5],i;
+scanf("%s",&s);
+
+{
+if((s[i]>'a')&&(s[i]<'z'))
+      {
+         printf("no");
+
+       }
+ else
+     {
+    printf("yes",s[i]);
+     }
+}}
