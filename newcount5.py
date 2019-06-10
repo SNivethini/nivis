9acb8f834408c758ec9a2eb7179f0aec76fbc9eb
SNivethini <46355009+SNivethini@users.noreply.github.com>
2019-06-10 20:44:53 +0530

    Update newcount5.py

--- newcount5.py
+++ newcount5.py
@@ -1,6 +1,7 @@
 string=input()
 n=0
 for i in range(len(string)):
-if(string[i].isalpha()!=True and string[i].isdigit()!=True):
-n=n+1
+  if(string[i].isalpha()!=True and string[i].isdigit()!=True):
+    n=n+1
 print(n)
+  
