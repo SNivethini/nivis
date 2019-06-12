6fc69df59ccdcf44b95f88a23e014f83c8ee527e
SNivethini <46355009+SNivethini@users.noreply.github.com>
2019-06-12 18:13:00 +0530

    Create hunt1.py

---
+++ hunt1.py
@@ -0,0 +1,14 @@
+a=int(input())
+n=list(map(int,input().split()))
+l=[]
+for x in range(a):
+    for i in range(x+1,len(n)):
+        if(n[i]==n[x]):
+          l.append(n[x])
+if (len(l)==0):
+    print("unique")
+else:
+    l.sort()
+    l_set=set(l)
+    for x in l_set:
+        print(x,end=" ")
